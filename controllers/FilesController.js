dd0dd60b1d4f50cb4962a5c2ede885f4b9d6a547
Adejare <adejare.arasheed@gmail.com>
2024-08-04 20:00:51 +0100

    Files Managers

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -6,18 +6,22 @@ const redisClient = require('../utils/redis');
 const dbClient = require('../utils/db');
 
 class FilesController {
+  static async tokenForUser(token) {
+    let user;
+    try {
+      const tokenKey = `auth_${token}`;
+      const userId = await redisClient.get(tokenKey);
+      user = await dbClient.db.collection('users').findOne({ _id: new ObjectId(userId) });
+    } catch (err) {
+      // eslint-disable-next-line no-empty
+    }
+    return user;
+  }
+
   static async postUpload(req, res) {
     const token = req.header('x-token'); // express format;
-    if (!token) {
-      return res.status(401).json({ error: 'Unauthorized' });
-    }
-    const tokenKey = `auth_${token}`;
-    const userId = await redisClient.get(tokenKey);
-    if (!userId) {
-      return res.status(401).json({ error: 'Unauthorized' });
-    }
+    const user = await FilesController.tokenForUser(token);
 
-    const user = await dbClient.db.collection('users').findOne({ _id: new ObjectId(userId) });
     if (!user) {
       return res.status(401).json({ error: 'Unauthorized' });
     }
@@ -54,27 +58,30 @@ class FilesController {
       }
     }
 
+    const newFile = ({
+      userId: user._id, // Associate file to the owner who created it
+      name,
+      type,
+      parentId,
+      isPublic,
+    });
+
     if (type === 'folder') {
       // Create and save the folder document in the DB
-      const newFolder = ({
-        userId: user._id, // Associate file to the owner who created it
-        name,
-        type,
-        parentId,
-        isPublic,
-      });
       try {
-        const result = await files.insertOne(newFolder);
-        // Destructure to exclude '_id' automatically inserted after inserted by MongoDB
-        const { _id, ...newFolderWithoutId } = newFolder;
+        const result = await files.insertOne(newFile);
+        // Destructure to exclude '_id' which was automatically inserted by insertOne
+        const { _id, ...newFileWithoutId } = newFile;
         return res.status(201).json({
           id: result.insertedId, // the file _id given in the file collection
-          ...newFolderWithoutId,
+          ...newFileWithoutId, // using spread operator
         });
       } catch (err) {
-        console.log(err);
+        return res.status(500).json({ error: 'Error inserting file' });
       }
     }
+
+    // Create a file in given or /tmp/files_manager path
     const folderPath = process.env.FOLDER_PATH || '/tmp/files_manager';
     if (!fs.existsSync(folderPath)) {
       fs.mkdirSync(folderPath, { recursive: true });
@@ -82,29 +89,20 @@ class FilesController {
     const fileName = uuid.v4();
     const filePath = path.join(folderPath, fileName);
 
-    // Decode Data written in base64 and write the file to disk
+    // Decode Data (file or image) written in base64 and write to filePath
     // const fileData = base64.decode(data);
-
     const fileData = Buffer.from(data, 'base64');
     fs.writeFileSync(filePath, fileData);
 
-    // Create and save the file document in the DB
-    const newFile = {
-      userId: user._id,
-      name,
-      type,
-      isPublic,
-      parentId,
-      localPath: filePath,
-    };
+    // Now, add localPath to newFile and write to the file DB itself
+    newFile.localPath = filePath;
     const file = await files.insertOne(newFile);
 
-    // Destructure to exclude the localPath property before spreading
-    // remove _id because it is automatically added by mongoDB
-    const { localPath, _id, ...newFileWithoutLoclaPath } = newFile;
+    // remove _id because it is automatically added by mongoDB insertOne
+    const { _id, localPath, ...newFileWithoutLocalPath } = newFile;
     return res.status(201).json({
       id: file.insertedId, // The id of the file just inserted
-      ...newFileWithoutLoclaPath,
+      ...newFileWithoutLocalPath,
     });
   }
 }
