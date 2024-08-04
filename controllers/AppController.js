9ca6d5f081e333ea7ec2f8912a8a3433e7063c5d
Adejare <adejare.arasheed@gmail.com>
2024-08-04 09:45:17 +0100

    Files Manager

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,18 +1,6 @@
 const redisClient = require('../utils/redis');
 const dbClient = require('../utils/db');
 
-async function connect() {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      if (redisClient.isAlive() && dbClient.isAlive()) {
-        resolve(true);
-      } else {
-        resolve(false);
-      }
-    }, 1000);
-  });
-}
-
 class AppController {
   static async getStatus(req, res) {
     const redisIsAlive = await redisClient.isAlive();
@@ -24,22 +12,6 @@ class AppController {
       redis: redisIsAlive,
       db: dbIsAlive,
     });
-    // let retry = 0;
-    // const repeatFunc = async () => {
-    //   retry += 1;
-    //   const result = await connect();
-    //   if (result) {
-    //     res.status = 200;
-    //     return res.status(200).send(JSON.stringify({ redis: true, db: true }));
-    //   } if (retry <= 10) {
-    //     return repeatFunc(); // Await the recursive call
-    //   }
-    //   return res.status(500).json({
-    //     redis: redisClient.isAlive(),
-    //     db: dbClient.isAlive(),
-    //   });
-    // };
-    // return repeatFunc();
   }
 
   static async getStats(req, res) {
