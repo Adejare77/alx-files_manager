c3800809f42fc189d276acd810807e4dced1dec3
Adejare <adejare.arasheed@gmail.com>
2024-08-06 21:02:43 +0100

    Files Manager

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 
-// const FilesController = require('../controllers/FilesController');
+const FilesController = require('../controllers/FilesController');
 const AppController = require('../controllers/AppController');
-// const AuthController = require('../controllers/AuthController');
-// const UsersController = require('../controllers/UsersController');
+const AuthController = require('../controllers/AuthController');
+const UsersController = require('../controllers/UsersController');
 
 const router = express.Router();
 
@@ -11,24 +11,24 @@ router.get('/status', AppController.getStatus);
 
 router.get('/stats', AppController.getStats);
 
-// router.post('/users', UsersController.postNew);
+router.post('/users', UsersController.postNew);
 
-// router.get('/connect', AuthController.getConnect);
+router.get('/connect', AuthController.getConnect);
 
-// router.get('/disconnect', AuthController.getDisconnect);
+router.get('/disconnect', AuthController.getDisconnect);
 
-// router.get('/users/me', UsersController.getMe);
+router.get('/users/me', UsersController.getMe);
 
-// router.post('/files', FilesController.postUpload);
+router.post('/files', FilesController.postUpload);
 
-// router.get('/files/:id', FilesController.getShow);
+router.get('/files/:id', FilesController.getShow);
 
-// router.get('/files', FilesController.getIndex);
+router.get('/files', FilesController.getIndex);
 
-// router.put('/files/:id/publish', FilesController.putPublish);
+router.put('/files/:id/publish', FilesController.putPublish);
 
-// router.put('/files/:id/unpublish', FilesController.putUnpublish);
+router.put('/files/:id/unpublish', FilesController.putUnpublish);
 
-// router.get('/files/:id/data', FilesController.getFile);
+router.get('/files/:id/data', FilesController.getFile);
 
 module.exports = router;
