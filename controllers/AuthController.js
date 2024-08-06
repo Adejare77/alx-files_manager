3f6e542948ae81a820d19993e47c43358b90edc7
Adejare <adejare.arasheed@gmail.com>
2024-08-06 22:08:35 +0100

    Files Manager

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -12,8 +12,11 @@ class AuthController {
     }
 
     const base64Credentials = authHeader.split(' ')[1];
-    const decodedCredentials = Buffer.from(base64Credentials, 'base64').toString('utf8');
+    const decodedCredentials = Buffer.from(base64Credentials, 'base64').toString('ascii');
     const credentials = decodedCredentials.split(':');
+    if (credentials.length !== 2) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
     const [email, password] = credentials;
 
     const user = await dbClient.db.collection('users').findOne({ email, password: sha1(password) });
